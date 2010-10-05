9f43c0ba377e2371404b099b55d7bc94440678e4
FrancoB411 <Franco@temedigital.com>
2010-10-05 13:51:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunflower::Application.config.secret_token = 'f62be7f6d98d7f74275ab6409c8b4df92a9d0ce4af53a1af5e7eec4558fe6e10c030b216aecfe7070bfbbc4821eadd508045f91678f68d2a9af9993e4a0efb31'
