d535284b3f1098c4f585b4c7ccb32a06011d76bc
ASAP <asap@briteideas.com>
2010-10-15 17:32:15 -0400

    [ALX] Added backend validation to Invitation Model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,7 @@
 class Invitation < ActiveRecord::Base
+  validates_uniqueness_of :email
+  validates_presence_of   :email
+  validates_format_of     :email,
+                          :with       => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+                          :message    => 'email must be valid'
 end
